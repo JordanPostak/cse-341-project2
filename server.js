753cc4de8b0c2e130a3195236dac9b53d8e4dd36
JordanPostak <jordan_postak@hotmail.com>
2024-01-27 18:48:07 -0700

    cors back on, didn't solve problem

--- server.js
+++ server.js
@@ -2,40 +2,59 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('passport');
-const GitHubStrategy = require('passport-github2').Strategy;
 const mongodb = require('./data/database');
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-}));
+app
+    .use(bodyParser.json())
+    .use(session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: true,
+    }))
+    // This is the basic express session({..}) initialization.
+    .use(passport.initialize())
+    // init passport on every route call.
+    .use(passport.session())
+    // allow passport to use "express-session".
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
+        );
+        res.setHeader(
+            'Access-Control-Allow-Methods', 
+            'POST, GET, PUT, PATCH, OPTIONS, DELETE'
+        );
+        next();
+    })
+    .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
+    .use(cors({ origin: ['http://localhost:3000', 'http://project2-tltm.onrender.com', 'https://project2-tltm.onrender.com'], credentials: true }))
+    .use("/", require("./routes/index.js"));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Your routes configuration
-app.use("/", require("./routes/index.js"));
-
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
-}, (accessToken, refreshToken, profile, done) => {
-    // User.findOrCreate({ githubId: profile.id }, function (err, user) {
-    return done(null, profile);
-}));
+    passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: process.env.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, done) {
+        //User.findOrCreate({ githubId: profile.id }, function (err, user) {
+            return done(null, profile);
+        //})
+    }
+    ));
 
 passport.serializeUser((user, done) => {
     done(null, user);
 });
-
 passport.deserializeUser((user, done) => {
     done(null, user);
 });
@@ -54,11 +73,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: '/api-docs', session: false
-}), (req, res) => {
-    req.session.user = req.user;
-    res.redirect('/');
-});
+    failureRedirect: '/api-docs', session: false}),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/');
+    });
 
 mongodb.initDb((err) => {
     if (err) {
